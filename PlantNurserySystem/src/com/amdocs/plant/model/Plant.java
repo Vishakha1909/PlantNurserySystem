8ebe0bb7ccc8f1f2d48f7f9ef69750645bc047ea
Vishakha Kumaresan <vishakhakumaresan2001@gmail.com>
2023-09-26 11:28:59 +0530

    Add files via upload

---
+++ PlantNurserySystem/src/com/amdocs/plant/model/Plant.java
@@ -0,0 +1,82 @@
+package com.amdocs.plant.model;
+
+import java.util.*;
+
+public class Plant {
+	int plantID;
+	String plantName;
+	String originCountryName;
+	int sunlightRequired;
+	String waterSupplyFrequency;
+	String plantType;
+	double cost;
+	
+	public Plant(int plantID, String plantName, String originCountryName, int sunlightRequired,
+			String waterSupplyFrequency, String plantType, double cost) {
+		super();
+		this.plantID = plantID;
+		this.plantName = plantName;
+		this.originCountryName = originCountryName;
+		this.sunlightRequired = sunlightRequired;
+		this.waterSupplyFrequency = waterSupplyFrequency;
+		this.plantType = plantType;
+		this.cost = cost;
+	}
+
+	public int getPlantID() {
+		return plantID;
+	}
+
+	public void setPlantID(int plantID) {
+		this.plantID = plantID;
+	}
+
+	public String getPlantName() {
+		return plantName;
+	}
+
+	public void setPlantName(String plantName) {
+		this.plantName = plantName;
+	}
+
+	public String getOriginCountryName() {
+		return originCountryName;
+	}
+
+	public void setOriginCountryName(String originCountryName) {
+		this.originCountryName = originCountryName;
+	}
+
+	public int getSunlightRequired() {
+		return sunlightRequired;
+	}
+
+	public void setSunlightRequired(int sunlightRequired) {
+		this.sunlightRequired = sunlightRequired;
+	}
+
+	public String getWaterSupplyFrequency() {
+		return waterSupplyFrequency;
+	}
+
+	public void setWaterSupplyFrequency(String waterSupplyFrequency) {
+		this.waterSupplyFrequency = waterSupplyFrequency;
+	}
+
+	public String getPlantType() {
+		return plantType;
+	}
+
+	public void setPlantType(String plantType) {
+		this.plantType = plantType;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	
+}
